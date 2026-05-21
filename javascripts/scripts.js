0b56291300280522722a46079dc7115595846509
ryletskaya2492 <glassssstorm@gmail.com>
2026-05-21 14:13:42 +0300

    update

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,9 @@
 let burger = document.querySelector(".burger");
 let nav = document.querySelector(".nav");
+let bask = document.querySelector(".bask");
 
 burger.onclick = function () {
   burger.classList.toggle("open");
   nav.classList.toggle("open");
+  bask.classList.toggle("hide");
 };
